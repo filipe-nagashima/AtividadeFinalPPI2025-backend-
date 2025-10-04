119c93650afbc10a3dee7dcb9fffb1c64763de29
Filipe <filipe.nagashima@outlook.com>
2025-10-04 17:05:49 -0300

    Implementação dos Controllers, Routes e ajuste no index

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import clienteRouter from './Routes/rotaCliente.js'
+import livroRouter from './Routes/rotaLivro.js'
 
 const hostname = '0.0.0.0'
 const porta = 4000
 
 const app = express()
+
+app.use(express.json())
+
+app.use("/cliente", clienteRouter)
+app.use("/livro", livroRouter)
 
 app.listen(porta, hostname, () => {
     console.log(`Servidor rodando em http://${hostname}:${porta}/`)
